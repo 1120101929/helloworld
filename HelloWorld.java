238efe46379e097aab27c15fbb79441835bd7fec
liuyimin <1061524626@qq.com>
2017-01-08 00:52:30 +0800

    resolve conflicts

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld{
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+<<<<<<< HEAD
 		System.out.println("My name is liuyimin");
+=======
+>>>>>>> parent of 51e15f5... add print hello world
 	}
 }
